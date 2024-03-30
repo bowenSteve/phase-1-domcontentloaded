2e4e35e30f7e5ffd1567eb746b05fe990cd764f1
Stephen Bowen <bowensteven17@gmail.com>
2024-03-30 08:47:38 +0300

    Complete domcontentloaded lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded',()=>{
+    let myElement = document.getElementById('text');
+    myElement.textContent="This is really cool!"
+      
+})
